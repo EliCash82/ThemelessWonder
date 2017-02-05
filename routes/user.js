1854e15906defe431937c6d79bfd95bb6b8943d1
Anderson <almostanderson@gmail.com>
2017-02-05 13:53:12 -0500

    Added login functionality

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
@@ -22,6 +23,35 @@ router.post('/', function (req, res, next) {
       message: 'Now you are official, CONGRATS, NO REALLY CONGRATULATIONS.',
       obj: result
     });
+  });
+});
+
+router.post('/signin', function (req,res,next) {
+  User.findOne({email: req.body.email}, function(err, user) {
+    if (err) {
+      return res.status(500).json({
+        title: 'You screwed up!  We take NO responsibility for this error!',
+        error: err
+      });
+    }
+    if (!user) {
+      return res.status(401).json({
+        title: 'No user found',
+        error: { message: 'THERE IS NO USER!  Do you not get this?!  THERE IS NO USER!'}
+      });
+    }
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      return res.status(401).json({
+        title: 'You failed',
+        error: { message: 'We are very disappointed in you.  I think you know why.'}
+      });
+    }
+    var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+    res.status(200).json({
+      message: 'Well done, soldier.  Welcome back to the trenches!',
+      token: token,
+      userId: user._id
+    })
   });
 });
 
